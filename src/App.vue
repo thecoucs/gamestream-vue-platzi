0395e7a9bbbabf8f9d32b23a47feb000368bf78d
edevars <e.devars.dev@gmail.com>
2025-01-21 16:01:49 -0600

    feat: fetching api

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,32 @@
 <script setup>
-import LayoutHero from './components/Layout/LayoutHero.vue';
+import LayoutHero from './components/Layout/LayoutHero.vue'
+import { onMounted, reactive } from 'vue'
+
+const API_URL = 'https://gamestreamapi.herokuapp.com/api/games'
+
+const state = reactive({
+  error: null,
+  isLoading: false,
+  data: [],
+})
+
+const fetchGames = async () => {
+  try {
+    state.isLoading = true
+    const response = await fetch(API_URL)
+    const json = await response.json()
+    state.data = json
+  } catch (error) {
+    console.error(error)
+    state.error = error
+  } finally {
+    state.isLoading = false
+  }
+}
+
+onMounted(() => {
+  fetchGames()
+})
 </script>
 
 <template>
